847acd19b86b89be5e4dde829cf303d8a8b3823a
Diego Fraga <diego.rfraga@gmail.com>
2020-02-28 19:02:44 -0400

    Add styles text / add config to Smartphone Change Activity Name

--- app/src/main/java/com/studio/nicedevice/MainActivity.java
+++ app/src/main/java/com/studio/nicedevice/MainActivity.java
@@ -1,9 +1,9 @@
 package com.studio.nicedevice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
 
 
-                Intent intent = new Intent(getApplicationContext(),Properties.class);
+                Intent intent = new Intent(getApplicationContext(), PropertiesActivity.class);
 
                 startActivity(intent);
                 finish();
@@ -34,5 +34,10 @@ public class MainActivity extends Activity {
         });
 
 
+    }
+
+
+    public static boolean isTablet(Context ctx){
+        return (ctx.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
 }
--- app/src/main/java/com/studio/nicedevice/PropertiesActivity.java
+++ app/src/main/java/com/studio/nicedevice/PropertiesActivity.java
@@ -1,6 +1,10 @@
 package com.studio.nicedevice;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -18,7 +22,7 @@ import static com.studio.nicedevice.R.id.textviewname;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class Properties extends Activity {
+public class PropertiesActivity extends Activity {
 
 
 
@@ -27,69 +31,82 @@ public class Properties extends Activity {
     TextView brandtextview;
     TextView parametersTextview;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_properties);
 
 
-        //Get getprop from device
-        executeCommandLine("getprop");
 
-        // Insert StringBuilder output to the String
-        //Need to update work only with the string stringoutput
-       // String stringoutput = String.valueOf(executeCommandLine("getprop"));
+        //Set orientation for Tablet or Smartphone
 
+        if(!isTablet(PropertiesActivity.this))
+            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
 
         // Project Name
 
         nametextview = findViewById(textviewname);;
         nametextview.setMovementMethod(new ScrollingMovementMethod());
-        String stringoutput = String.valueOf(executeCommandLine("getprop | grep ro.build.product;" +
-                "getprop | grep ro.mid_board;" +
-                "getprop | grep ro.product.board;" +
-                "getprop | grep ro.product.device;" +
-                "getprop | grep ro.product.model;" +
+        String stringoutput = String.valueOf(executeCommandLine("getprop | grep ro.mid_board;" +
+                "getprop | grep ro.build.product;" +
                 "getprop | grep ro.product.name;" +
+                "getprop | grep ro.product.board;" +
+                "getprop | grep ro.product.model;" +
+                "getprop | grep ro.product.device;" +
                 "getprop | grep ro.product.vendor.model"));
 
-        stringoutput = stringoutput.replace("[", "");
-        stringoutput = stringoutput.replace("]", "");
+        stringoutput = stringoutput.replace("[", "").replace("]", "");
+
+
+
         nametextview.setText(stringoutput);
 
         // Brand Name
 
         brandtextview = findViewById(textviewbrand);
         brandtextview.setMovementMethod(new ScrollingMovementMethod());
-        stringoutput = String.valueOf(executeCommandLine("getprop | grep ro.product.brand;" +
+        stringoutput = String.valueOf(executeCommandLine("getprop | grep ro.build.host;"+
+                "getprop | grep ro.product.brand;" +
                 "getprop | grep ro.product.manufacturer;" +
-                "getprop | grep ro.build.host;" +
                 "getprop | grep ro.product.vendor.manufacturer;"));
 
-        stringoutput = stringoutput.replace("[", "");
-        stringoutput = stringoutput.replace("]", "");
+        stringoutput = stringoutput.replace("[", "").replace("]", "");
         brandtextview.setText(stringoutput);
 
         // Parameters Project
 
         parametersTextview =findViewById(textViewparameters);
         parametersTextview.setMovementMethod(new ScrollingMovementMethod());
-        stringoutput = String.valueOf(executeCommandLine("getprop | grep ro.build.version.security_patch;" +
-                "getprop | grep ro.com.google.acsa;" +
+        stringoutput = String.valueOf(executeCommandLine("getprop | grep ro.vendor.mediatek.platform;" +
+                "getprop | grep ro.product.vendor.device;" +
+                "getprop | grep ro.build.fingerprint;" +
+                "getprop | grep ro.build.version.security_patch;" +
                 "getprop | grep ro.com.google.gmsversion;" +
                 "getprop | grep ro.product.first_api_level;" +
-                "getprop | grep persist.sys.usb.config;"+
                 "getprop | grep persist.sys.timezone;" +
                 "getprop | grep ro.product.locale;"+
                 "getprop | grep persist.sys.locale;" +
-                "getprop | grep ro.sf.lcd_density"));
+                "getprop | grep ro.sf.lcd_density" +
+                "getprop | grep persist.sys.usb.config;"+
+                "getprop | grep ro.com.google.acsa;"));
 
-        stringoutput = stringoutput.replace("[", "");
-        stringoutput = stringoutput.replace("]", "");
+        stringoutput = stringoutput.replace("[", "").replace("]", "");
         parametersTextview.setText(stringoutput);
 
 
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(PropertiesActivity.this,MainActivity.class);
+        //intent2.putExtra("tipo", vc_TipoEmitente);
+        //intent2.putExtra("mensagem", "");
+        startActivity(intent);
+        finish();
     }
 
     //function execute command line (String) return output
@@ -125,6 +142,11 @@ public class Properties extends Activity {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+// Check if the device is a tablet
+    public static boolean isTablet(Context ctx){
+        return (ctx.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
 
 }
