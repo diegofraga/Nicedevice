6e0f5d59eda56993a12014508faee31ba156dac2
Diego Fraga <diego.rfraga@gmail.com>
2020-03-04 17:56:24 -0400

    Add button Logs Main acitivity

--- app/src/main/java/com/studio/nicedevice/logsActivity.java
+++ app/src/main/java/com/studio/nicedevice/logsActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class logsActivity extends AppCompatActivity {
+public class logsActivity extends PropertiesActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logs);
+
+
+
+        executeCommandLine("adb logcat -c");
+
+        executeCommandLine("adb logcat ");
+
+
     }
 }
